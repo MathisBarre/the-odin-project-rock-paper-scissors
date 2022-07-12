549364a0e508cfe7658739144772c90a4c432551
Mathis Barré <m.barre@beapp.fr>
2022-07-12 12:42:37 +0200

    feat: getRandomIntFunction

---
+++ index.js
@@ -0,0 +1,7 @@
+/**
+ * Take a maximum integer and return another integer between 0 (included) and "max" (included)
+ * Derived from https://developer.mozilla.org/fr/docs/Web/JavaScript/Reference/Global_Objects/Math/random interactif exemple
+ */
+ function getRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
