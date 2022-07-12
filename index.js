8151c7fa8f5565cc09af99903fd087ba97a7afef
Mathis Barré <m.barre@beapp.fr>
2022-07-12 12:42:55 +0200

    feat: computerPlay function

--- index.js
+++ index.js
@@ -5,3 +5,11 @@
  function getRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
+
+/**
+ * Take no parameters and randomly return string "Rock", "Paper" or "Scissors"
+ */
+function computerPlay() {
+  const randomIntBetween0And2 = getRandomInt(2)
+  return ["Rock", "Paper", "Scissors"][randomIntBetween0And2]
+}
